c8ca10108790363218b733dae07fc08f3670bea2
Kurata0320 <otakura320@gmail.com>
2022-08-20 00:34:54 +0900

    commit

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"rss/model"
 
 	"github.com/labstack/echo"
 	"github.com/mmcdole/gofeed"
 )
 
 const GOOGLE_NEWS_RSS_URL = "https://news.google.com/rss/search"
+
+type Feed struct {
+	Title  string `json:"title"`
+	Link   string `json:"link"`
+	Word   string `json:"word"`
+	Source string `json:"source"`
+}
 
 func main() {
 	e := echo.New()
@@ -22,7 +28,7 @@ func main() {
 func rss(c echo.Context) error {
 	url := fmt.Sprintf("%s?q=%s&hl=ja&gl=JP&ceid=JP:ja", GOOGLE_NEWS_RSS_URL, url.QueryEscape("脆弱性"))
 	feed, err := gofeed.NewParser().ParseURL(url)
-	feeds := []model.Feed{}
+	feeds := []Feed{}
 
 	if err != nil {
 		return c.String(http.StatusExpectationFailed, "error")
@@ -31,7 +37,7 @@ func rss(c echo.Context) error {
 			if idx > 2 {
 				break
 			}
-			feeds = append(feeds, model.Feed{
+			feeds = append(feeds, Feed{
 				Title:  item.Title,
 				Link:   item.Link,
 				Word:   "脆弱性に関するニュース",
--- model/model.go
+++ model/model.go
@@ -1,8 +1,1 @@
 package model
-
-type Feed struct {
-	Title  string `json:"title"`
-	Link   string `json:"link"`
-	Word   string `json:"word"`
-	Source string `json:"source"`
-}
