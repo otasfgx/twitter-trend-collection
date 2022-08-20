cb699cc626fd8b0b6c45e26ced895cee7e67af90
Kurata0320 <otakura320@gmail.com>
2022-08-21 01:24:41 +0900

    msg

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"rss/infrastructure/datasource/trends"
 	"rss/model"
 	"rss/usecase"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
@@ -30,11 +31,12 @@ func main() {
 		TWITTER_API_SECRET    = os.Getenv("TWITTER_API_SECRET")
 		TWITTER_ACCESS_TOKEN  = os.Getenv("TWITTER_ACCESS_TOKEN")
 		TWITTER_ACCESS_SECRET = os.Getenv("TWITTER_ACCESS_SECRET")
+		port, _               = strconv.Atoi(os.Getenv("PORT"))
 	)
 	twitter := trends.NewTwitter(TWITTER_API_KEY, TWITTER_API_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
 	usecase := usecase.NewUseCase(twitter)
 	application := basic.NewApplication(usecase)
-	application.Run(8080)
+	application.Run(port)
 }
 
 func rss(c echo.Context) error {
