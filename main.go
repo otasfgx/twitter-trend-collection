f0cc8558bd3774d91949e6c86f80d90b5e0dd638
Kurata0320 <otakura320@gmail.com>
2022-08-20 10:39:16 +0900

    msg

--- main.go
+++ main.go
@@ -6,41 +6,37 @@ import (
 	"net/url"
 	"os"
 
+	"rss/model"
+
 	"github.com/labstack/echo"
 	"github.com/mmcdole/gofeed"
 )
 
 const GOOGLE_NEWS_RSS_URL = "https://news.google.com/rss/search"
 
-type Feed struct {
-	Title  string `json:"title"`
-	Link   string `json:"link"`
-	Word   string `json:"word"`
-	Source string `json:"source"`
-}
-
 func main() {
 	e := echo.New()
-	e.GET("/", rss)
+	e.POST("/", rss)
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
 
 func rss(c echo.Context) error {
-	url := fmt.Sprintf("%s?q=%s&hl=ja&gl=JP&ceid=JP:ja", GOOGLE_NEWS_RSS_URL, url.QueryEscape("脆弱性"))
+	word := c.FormValue("word")
+	url := fmt.Sprintf("%s?q=%s&hl=ja&gl=JP&ceid=JP:ja", GOOGLE_NEWS_RSS_URL, url.QueryEscape(word))
 	feed, err := gofeed.NewParser().ParseURL(url)
-	feeds := []Feed{}
+	feeds := []model.Feed{}
 
 	if err != nil {
-		return c.String(http.StatusExpectationFailed, "error")
+		return c.String(http.StatusOK, "error")
 	} else {
 		for idx, item := range feed.Items {
 			if idx > 2 {
 				break
 			}
-			feeds = append(feeds, Feed{
+			feeds = append(feeds, model.Feed{
 				Title:  item.Title,
 				Link:   item.Link,
-				Word:   "脆弱性に関するニュース",
+				Word:   word,
 				Source: "Google News",
 			})
 		}
--- model/model.go
+++ model/model.go
@@ -1,1 +1,8 @@
 package model
+
+type Feed struct {
+	Title  string `json:"title"`
+	Link   string `json:"link"`
+	Word   string `json:"word"`
+	Source string `json:"source"`
+}
