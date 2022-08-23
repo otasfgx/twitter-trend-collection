3c667d5c31d3673db95d1a36355d7be638528f39
otasfgx <otakura320@gmail.com>
2022-08-23 19:13:50 +0900

    msg

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
 	"rss/infrastructure/application/basic"
 	"rss/infrastructure/datasource/trends"
-	"rss/model"
 	"rss/usecase"
 	"strconv"
 
 	"github.com/joho/godotenv"
-	"github.com/labstack/echo"
-	"github.com/mmcdole/gofeed"
 )
-
-const GOOGLE_NEWS_RSS_URL = "https://news.google.com/rss/search"
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -37,28 +29,4 @@ func main() {
 	usecase := usecase.NewUseCase(twitter)
 	application := basic.NewApplication(usecase)
 	application.Run(port)
-}
-
-func rss(c echo.Context) error {
-	word := c.FormValue("word")
-	url := fmt.Sprintf("%s?q=%s&hl=ja&gl=JP&ceid=JP:ja", GOOGLE_NEWS_RSS_URL, url.QueryEscape(word))
-	feed, err := gofeed.NewParser().ParseURL(url)
-	feeds := []model.Feed{}
-
-	if err != nil {
-		return c.String(http.StatusOK, "error")
-	} else {
-		for idx, item := range feed.Items {
-			if idx > 0 {
-				break
-			}
-			feeds = append(feeds, model.Feed{
-				Title:  item.Title,
-				Link:   item.Link,
-				Word:   word,
-				Source: "Google News",
-			})
-		}
-	}
-	return c.JSON(http.StatusOK, feeds)
 }
